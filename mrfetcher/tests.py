d2b503c8d8cd14819c7007276ca38d5357020740
Ben Last <ben@benlast.com>
2015-06-08 16:39:44 -0700

    Add a random string method to make testing easier

--- mrfetcher/tests.py
+++ mrfetcher/tests.py
@@ -1,12 +1,33 @@
+import random
+import string
 from unittest import TestCase
 
 import mock
 
 
+def random_string(
+        choose_from=None,
+        length=8
+):
+    """
+    Return a random sequence of characters
+    :param choose_from: the set of eligible characters - by default
+    the set is string.ascii_lowercase + string.digits
+    :param length: the length of the sequence to be
+    returned
+    :return: the sequence
+    """
+    choices = choose_from or (string.ascii_lowercase + string.digits)
+    return ''.join(
+        random.choice(choices)
+        for _ in xrange(length)
+    )
+
 class MockPOP(object):
     connected = False
     logged_in = False
 
+    # Each message is a tuple of (headers, body)
     MESSAGES = [
 
     ]
