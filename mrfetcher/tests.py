c46203d36e3aaff01ac23336355a7d23340a2d19
Ben Last <ben@benlast.com>
2015-06-08 16:35:49 -0700

    Start of mock POP3 server, committed as WIP

--- mrfetcher/tests.py
+++ mrfetcher/tests.py
@@ -3,6 +3,28 @@ from unittest import TestCase
 import mock
 
 
+class MockPOP(object):
+    connected = False
+    logged_in = False
+
+    MESSAGES = [
+
+    ]
+
+    def __init__(self, host, port):
+        assert not self.connected
+        self.connected = True
+        self.user = None
+        self.password = None
+
+    def user(self, user):
+        self.user = user
+        return '+OK password required for user "%s"' % user
+
+    def pass_(self, password):
+        assert self.user is not None
+        return
+
 class TestPOP3(TestCase):
     def test_login(self):
         pass
